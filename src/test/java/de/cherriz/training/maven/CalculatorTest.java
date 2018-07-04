1b8e9d03fc1f8e65a6b6fdf9e401ad70871f681d
Benjamin Nothdurft <b.nothdurft@gmail.com>
2018-07-04 13:29:59 +0200

    Run with JUnit 5.

--- src/test/java/de/cherriz/training/maven/CalculatorTest.java
+++ src/test/java/de/cherriz/training/maven/CalculatorTest.java
@@ -1,38 +1,30 @@
 package de.cherriz.training.maven;
 
-import org.junit.Assert;
-import org.junit.Test;
+//import org.junit.Assert;
+//import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
 	@Test
 	public void testSum() {
-		// Given
 		Calculator calculator = new Calculator();
-		// When
-		int result = calculator.sum(2, 2);
-		// Then
-		if (result != 4) {   // if 2 + 2 != 4
-			Assert.fail();
-		}
+		assertEquals(4, calculator.sum(2, 2));
+
 	}
 
 	@Test
 	public void testMinus() {
 		Calculator calculator = new Calculator();
-		Assert.assertEquals(0, calculator.minus(2, 2));
+		assertEquals(0, calculator.minus(2, 2));
 	}
 
 	@Test
 	public void testDivide() {
 		Calculator calculator = new Calculator();
-		Assert.assertEquals(2, calculator.divide(6, 3));
+		assertEquals(2, calculator.divide(6, 3));
 	}
-
-	@Test(expected = ArithmeticException.class)
-	public void testDivideWillThrowExceptionWhenDivideOnZero() {
-		Calculator calculator = new Calculator();
-		calculator.divide(6, 0);
-	}
-
 }
